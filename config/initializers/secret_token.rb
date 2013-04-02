d12d0d4232ef4f9ffe755f6ae1d5abba33947fab
Donavan White <digi.cazter@gmail.com>
2013-04-01 18:55:32 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmnishipExample::Application.config.secret_token = '29e5b5bfca193d2996d3817b13ed3252735fd598433b370dce6c23360baa0b00185883817d83c731e30384817078eeac7cae479788ec7f42b1435d20b1201a8f'
